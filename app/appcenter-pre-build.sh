eb183d1efa243087daf9661c3868205d6eda008c
Miguel Rodriguez <miguelr@globius.org>
2018-02-12 01:08:21 -0500

    The meme

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-
+echo PreScript Start
+cat /etc/*-release
+ip addr
+uname -a
+hostname
+cat /etc/passwd
+echo Ok, heres the real part of the script
 pwd
+pwd > $MMBuildDir
+cd ..
 make pre-run
